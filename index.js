e854bfdffdc06177bb2fed19a1d59e237d354e48
Nekun93 <nguungokung@gmail.com>
2025-11-05 14:58:29 +0800

    first entry of the webpage

--- index.js
+++ index.js
@@ -23,15 +23,6 @@ async function runCode() {
     
     // await delay(1000);
     // console.log('Final 1 second delay');
-
-    // WITHOUT await: Everything happens at once
-setTimeout(() => console.log('1'), 1000);
-console.log('2'); // Prints immediately, before the setTimeout
-
-// WITH await: Things happen in order
-await delay(1000); // WAIT here
-console.log('1');  // Then print 1
-console.log('2');  // Then print 2
 }
 
 runCode();
